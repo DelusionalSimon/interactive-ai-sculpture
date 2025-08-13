f16b37f83c29864ec4ee453c399fb8ceeb73dcaa
simon håkansson <simon@delusional.enterprises>
2025-08-13 15:04:44 +0200

    feat(firmware):  set up a basic movement test for one servo

--- firmware/interactive-sculpture-firmware/src/main.cpp
+++ firmware/interactive-sculpture-firmware/src/main.cpp
@@ -47,12 +47,32 @@ void setup() {
   
   // Initialize the PCA9685 servo driver.
   pwm.begin();
-  pwm.setPWMFreq(50);
+  pwm.setPWMFreq(SERVO_FREQUENCY);
 
 }
 
 //-------------[ MAIN LOOP ]-------------
 void loop() {
+
+  float sinValueLeaf1 = sin(phaseLeaf1);
+
+  // Get the angle the leaf should have, with higher resolution
+  int angleLeaf1 = map(sinValueLeaf1*1000, -1000, 1000, LEAF_MIN_ANGLE*10, LEAF_MAX_ANGLE*10);
+  // TODO : write a mapping function to use the full resolution of the servo
+
+  // Convert the angle to pulse width
+  int pulseWidthLeaf1 = map(angleLeaf1, 0, SERVO_MAX_ANGLE*10, PULSEWIDTH_MIN, PULSEWIDTH_MAX);
+  
+  // Set the servo position
+  pwm.writeMicroseconds(SERVO_LEAF_1, pulseWidthLeaf1);
+
+  // Increment the phase for the next iteration
+  phaseLeaf1 += speedLeaf1;
+
+  // Reset the phase if it exceeds 2*PI to avoid overflow
+  if (phaseLeaf1 >= 2 * PI) {
+    phaseLeaf1 -= 2 * PI;
+  }
 
 }
 
