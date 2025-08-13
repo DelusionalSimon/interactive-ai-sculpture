c872eb1bc38e47bd58b168dfc7f1250b6626d67a
simon håkansson <simon@delusional.enterprises>
2025-08-13 11:04:38 +0200

    docs(firmware): add file header

--- firmware/interactive-sculpture-firmware/src/main.cpp
+++ firmware/interactive-sculpture-firmware/src/main.cpp
@@ -1,3 +1,20 @@
+/******************************************************************************
+ *
+ * File:        main.c
+ * Author:      Simon Håkansson
+ * Date:        2025-08-13
+ *
+ * Description: Firmware for handling sensor inputs and controlling 
+ * the mechatronic leaves of the GIBCA 2025 interactive sculpture. 
+ * Interfaces with a host computer over serial to initialize 
+ * interaction and receive commands based on AI analysis.
+ *
+ * Copyright (c) 2025 Simon Håkansson
+ *  
+ * This software is released under the MIT License.
+ * See the LICENSE file in the project root for the full license text.
+ *
+ *****************************************************************************/
 #include <Arduino.h>
 #include <Adafruit_PWMServoDriver.h>
 
