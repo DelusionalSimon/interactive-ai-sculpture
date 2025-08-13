7b497a8f390b6fa696a7cd822f02017ca334b6c3
simon håkansson <simon@delusional.enterprises>
2025-08-13 12:47:56 +0200

    feat(firmware): add initial setup and configuration for servo control
    - Includes Adafruit PWM Servo Driver library.
    - Defines constants for servo pulse width, angle limits, and frequency.
    - Initializes the PCA9685 driver in the setup function.
    - Establishes global variables for leaf animation parameters as a starting point.

--- firmware/interactive-sculpture-firmware/src/main.cpp
+++ firmware/interactive-sculpture-firmware/src/main.cpp
@@ -1,4 +1,4 @@
-/******************************************************************************
+/*****************************************************************************
  *
  * File:        main.c
  * Author:      Simon Håkansson
@@ -15,14 +15,43 @@
  * See the LICENSE file in the project root for the full license text.
  *
  *****************************************************************************/
+//-------------[ LIBRARIES ]-------------
 #include <Arduino.h>
 #include <Adafruit_PWMServoDriver.h>
+#include <Wire.h>
 
+//-------------[ CONFIGURATION ]-------------
+// Create an instance of the Adafruit_PWMServoDriver class
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
+// Define motor outputs on the PCA9685
+#define SERVO_LEAF_1 0 // testing with one leaf first
+
+// Define the constants for the servos used 
+#define PULSEWIDTH_MIN 500 
+#define PULSEWIDTH_MAX 2500 
+#define SERVO_MAX_ANGLE 270
+#define SERVO_FREQUENCY 50 
+
+// Define the movement range for the leaves to not break the sculpture
+#define LEAF_MIN_ANGLE 45
+#define LEAF_MAX_ANGLE 135
+
+// Animation parameters to make each leaf move independently
+float speedLeaf1 = 0.015;
+float phaseLeaf1 = 0.0;
+// TODO: Add more leaves and their parameters
+
+//-------------[ SETUP FUNCTION ]-------------
 void setup() {
+  
+  // Initialize the PCA9685 servo driver.
+  pwm.begin();
+  pwm.setPWMFreq(50);
 
 }
 
+//-------------[ MAIN LOOP ]-------------
 void loop() {
 
 }
