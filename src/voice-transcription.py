a271b931cfe45b7b8dad2a23aaedc30589434534
simon håkansson <simon@delusional.enterprises>
2025-08-23 20:43:04 +0200

    feat(ai-pipeline): implement whisper transcription and audio deletion

--- src/voice-transcription.py
+++ src/voice-transcription.py
@@ -17,17 +17,27 @@ import sounddevice as sd
 from scipy.io.wavfile import write
 from pathlib import Path
 import os
+import whisper
 
-# TODO: add the whisper model once audio recording is working
 
 # -------------[ CONFIGURATION ]-------------
+# Audio recording settings
 SAMPLE_RATE = 16000     # Whisper requires 16kHz sample rate
 DURATION = 5            # Recording duration in seconds
 OUTPUT_FOLDER = "output"
 RECORDING_FILENAME = "temp_recording.wav"
 
+# Whisper model settings
+WHISPER_MODEL = "base.en" # Let's use a smaller model for faster processing during testing
+
+# -------------[ INITIALIZATION ]-------------
+print("Loading Whisper model...")
+model = whisper.load_model(WHISPER_MODEL)
+print("Model loaded.")
+
+
 # -------------[ FUNCTIONS ]-------------
-def record_audio() -> str:
+def record_audio() -> Path:
     """
     @brief  Records audio from the microphone for a fixed duration.
 
@@ -43,12 +53,12 @@ def record_audio() -> str:
     script_dir = Path(__file__).parent.parent
     
     # Define the output path relative to the script's location
-    output_dir =  script_dir / OUTPUT_FOLDER
+    output_dir = script_dir / OUTPUT_FOLDER
     
     # Create the output folder if it doesn't exist.
     output_dir.mkdir(parents=True, exist_ok=True)
     
-    output_path = output_dir / RECORDING_FILENAME    
+    output_path = output_dir / RECORDING_FILENAME     
 
     # Use the `sd.rec()` function to record.
     print("Recording...")
@@ -61,6 +71,32 @@ def record_audio() -> str:
 
     return output_path
 
+def transcribe_audio(path: str, model) -> str:
+    """
+    @brief  Transcribes audio from an input audio file using a Whisper model.
+
+    @details This function takes the location of an audio file as input
+             and .uses a local Whisper model to transcribe the audio content
+             into text. The transcribed text is returned as a string.
+
+    @todo Save the text to a file or database.
+    @todo Make the model work on GPU for faster processing (fp16).
+
+    @param path The file path of the audio recording to transcribe.
+    @param model The Whisper model object to use for transcription.
+    @return The transcribed text as a string.
+    """
+    print("Transcribing audio...")
+    result = model.transcribe(path)
+    transcription = result["text"]
+    print("Transcription complete.")
+
+    os.remove(path)  # Remove the temporary audio file for privacy.
+    print("Temporary audio file deleted.")
+    return transcription
+
 if __name__ == "__main__":
     audio_file_path = record_audio()
     print(f"Audio saved to: {audio_file_path}")
+    transcription = transcribe_audio(str(audio_file_path), model)
+    print(f"Transcription: {transcription}")
